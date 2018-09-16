271635ccd5ed45f40bf2225026df70f1ea062673
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-16 09:40:57 -0400

    Added user schema

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Joi = require('joi');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    bill: billSchema,
+    categories: [categorySchema],
+    events: [eventSchema]
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(userData) {
+    const user = {
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(8).required()
+    };
+
+    let result = Joi.validate(userData, user);
+    let { error } =  result;
+
+    return error;
+}
+
+
+module.exports = {
+  User,
+  validateUser
+};
