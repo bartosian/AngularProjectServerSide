f8db48bbc4091d3059b6bed9f7e4d2b4d4081553
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-16 14:37:56 -0400

    Added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const JWT = require('jsonwebtoken');
+
+
+function authToken(req, res, next) {
+    const token = req.header("x-auth-token");
+
+    if(!token) {
+        return req.status(401)
+            .json({
+                "message": "Access denied. No token provided!"
+            });
+    }
+
+    try {
+        const decObject = JWT.verify(token, process.env.JWTKEY);
+
+        req.user = decObject;
+
+        next();
+    } catch(ex) {
+        return res.status(401)
+            .json({
+                "message": "You don't have permission on this operation!"
+            });
+
+    }
+
+}
+
+module.exports = authToken;
